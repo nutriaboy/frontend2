4842fba2f5ee1146408eb0fc54b27115547cfcd4
nutriaboy <fr.tapiam@gmail.com>
2023-05-16 23:56:54 -0400

    Mi primera app

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+
+export const App = () => {
+  return (
+    <View>
+      <Text>tu mamita sexo¿?¿?</Text>
+
+
+
+    </View>
+    
+  )
+}
